515120711e3ae446bb2edce1f587df944c8abc67
MIMER17 <mihirshetty9@gmail.com>
2021-07-01 22:51:50 +0530

    done

---
+++ quiz/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Home from "./Components/Home";
+import Quiz from "./Components/Quiz";
+import Result from "./Components/Result";
+function App() {
+  return (
+    <div className="App">
+      <Home/>
+      <Quiz/>
+      <Result/>
+    </div>
+  );
+}
+
+export default App;
